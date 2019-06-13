8df1260dd7c04d9ef0bd1b30f1bdd4a7e3dbdc10
jerrycook356 <Jerrycook356@gmail.com>
2019-06-13 10:39:07 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {WarningAlertComponent} from './warningAlert/warningAlert.component';
+import {SuccessAlertComponent} from './successAlert/successAlert.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
